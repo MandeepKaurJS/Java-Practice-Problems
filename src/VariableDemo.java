617fcb5e14b35cca543b86ad3c07d50d7b2ba361
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-09-03 12:49:23 -0700

    update

--- src/VariableDemo.java
+++ src/VariableDemo.java
@@ -2,5 +2,7 @@ public class VariableDemo {
     public static void main(String[] args){
         int x=20;
         System.out.println("I'm showing you  the x value on console "+x);
+        x=25;
+        System.out.println("I shows the new value of x " +x);
     }
 }
