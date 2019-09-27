95426277d3ca7786e9926a59202182a51b30956e
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-09-26 21:59:47 -0700

    Adding some input statement

--- src/TripPlanner.java
+++ src/TripPlanner.java
@@ -1,3 +1,5 @@
+import com.sun.deploy.util.SyncAccess;
+
 import java.util.Scanner;
 public class TripPlanner {
     public static void main(String[] args){
@@ -7,7 +9,14 @@ public class TripPlanner {
         String name=input.next();
         System.out.print("Nice to meet you "+name+",where are you travelling to?");
         String country=input.next();
-        System.out.print("");
+        System.out.print("Great! " +country+ " City Sounds Like a Great Trip.\n +++++++++++\n\n");
+        System.out.print("How many days are you going to spend travelling ?");
+        String days=input.next();
+        System.out.print("How much money, in USD, are you planning to spend on your trip?");
+        int money=input.nextInt();
+        System.out.print("What is the three letter curreny Symbol for your travel destination?");
+        String symbol=input.next();
+
     }
 }
 
