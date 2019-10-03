bb3afea611e8a6758a22f5fc1893b5f577bb7e33
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-10-03 15:49:08 -0700

    Added new java class for sorting strings

---
+++ src/SortingStrings.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SortingStrings {
+    public static void main(String[] args){
+        int count;
+        Scanner input=new Scanner(System.in);
+        count=input.nextInt();
+        
+    }
+}
+
+
--- src/SquareRoot.java
+++ src/SquareRoot.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
+
 public class SquareRoot {
     public static void main(String[] args){
-
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter any number: ");
+        int num=input.nextInt();
+        System.out.println("SqureRoot of this number is :"+squareroot(num));
     }
     public static double squareroot(int number){
         double temp;
         double Sqrt=number/2;
         do{
             temp=Sqrt;
-            Sqrt=(temp+(number/temp)/2);
+            Sqrt=(temp+(number/temp))/2;
         }while ((temp-Sqrt)!=0);
         return Sqrt;
     }
