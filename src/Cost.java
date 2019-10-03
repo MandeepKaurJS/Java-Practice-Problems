647bddc0c7adc32ba22d9c7b5fb317b57ac3acce
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-10-03 14:53:54 -0700

    Added for loop

--- src/Cost.java
+++ src/Cost.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 
 public class Cost {
     public static void main(String[] args){
-        Scanner input=new Scanner(System.in);
-        System.out.println("Enter any Number: ");
-        int num=input.nextInt();
-        if(num%3==0 && num%5==0){
-            System.out.println("FizzBuzz");
-        }else if(num%3==0){
-            System.out.println("Fizz");
-        }else if(num%5==0){
-            System.out.println("Buzz");
-        }else{
-            System.out.println("not Fizz nor Buzz");
+        //Scanner input=new Scanner(System.in);
+        //System.out.println("Enter any Number: ");
+        //int num=input.nextInt();
+        for(int num=0;num<100;num++) {
+            if (num % 3 == 0 && num % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (num % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (num % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println("not Fizz nor Buzz: "+num);
+            }
         }
     }
 }
