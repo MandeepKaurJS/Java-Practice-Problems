157030f1b06476e01579f73e3ef7499d2de8308c
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-10-03 14:27:41 -0700

    Added if condition

--- src/Cost.java
+++ src/Cost.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class Cost {
     public static void main(String[] args){
-        int[] cost={10,5,2,200,5,1};
-        for(int i=0;i<=cost.length;i++){
-           // System.out.println(cost[i]);
-            if(cost[i]<cost.length-1||cost[i]<cost.length-2){
-                System.out.println(cost[i]);
-            }
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter any Number: ");
+        int num=input.nextInt();
+        if(num%3==0 && num%5==0){
+            System.out.println("FizzBuzz");
+        }else if(num%3==0){
+            System.out.println("Fizz");
+        }else if(num%5==0){
+            System.out.println("Buzz");
+        }else{
+            System.out.println("not Fizz nor Buzz");
         }
     }
 }
