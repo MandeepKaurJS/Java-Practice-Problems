4fd0d7e6fa759c5cc3ff132ad3f1e2685a6bf539
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-10-03 13:26:44 -0700

    Added

--- src/Cost.java
+++ src/Cost.java
@@ -1,2 +1,8 @@
 public class Cost {
+    public static void main(String[] arga){
+        int cost[]={10,5,2,200,5,1};
+        for(int i=0;i<=cost.length;i++){
+            System.out.print(i);
+        }
+    }
 }
