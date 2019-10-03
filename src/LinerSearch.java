8537b44b9ea303be41fc9735dc697c5086fc81ac
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-10-03 16:56:11 -0700

    Adding new java class which shows the linear search process

--- src/LinerSearch.java
+++ src/LinerSearch.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class LinerSearch {
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
+        System.out.println("enter the size of Array: ");
         int count=input.nextInt();
         int array[]=new int[count];
         Scanner items=new Scanner(System.in);
+        System.out.println("Enter the element of array List: ");
         for(int i=0;i<count;i++){
             array[i]=items.nextInt();
         }
         System.out.println("Enter Searched Item: ");
         int search=input.nextInt();
         for(int i=0;i<count;i++){
-            if(array[i]==search){
-                System.out.println("Find the search value at"+array[i]+"Location");
-            }else if(array.length>search){
-                System.out.println("Value doesn't exist");
+            if(array[i]==search) {
+                System.out.println("Find the search value at" + i + "Location");
             }
             else{
                 System.out.println("Item not found");
