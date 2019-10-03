fc401b369ddd31eac8003f50fbf381b7de7bf2d2
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-10-03 16:22:03 -0700

    Added new java class for adding elements of array

--- src/SumofArrayElement.java
+++ src/SumofArrayElement.java
@@ -3,14 +3,20 @@ import java.util.Scanner;
 public class SumofArrayElement {
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
+        int sum=0;
+        System.out.println("Enter the number count: ");
         int count=input.nextInt();
         int array[]=new int[count];
+        Scanner input2=new Scanner(System.in);
+        System.out.println("Enter element one by one: ");
         for(int i=0;i<count;i++){
-            array[i]=input.nextInt();
+
+            array[i]=input2.nextInt();
         }
         for(int num:array){
-            int sum=0;
             sum=sum+num;
-        }
+
+        }System.out.println("Sum of array Elemnt: "+sum);
+
     }
 }
