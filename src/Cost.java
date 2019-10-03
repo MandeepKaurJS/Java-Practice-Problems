ae81688cb924ebe2b863ba1dd83bb2c2293c8b14
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-10-03 13:35:07 -0700

    Added for looop

--- src/Cost.java
+++ src/Cost.java
@@ -1,8 +1,11 @@
 public class Cost {
-    public static void main(String[] arga){
-        int cost[]={10,5,2,200,5,1};
+    public static void main(String[] args){
+        int[] cost={10,5,2,200,5,1};
         for(int i=0;i<=cost.length;i++){
-            System.out.print(i);
+           // System.out.println(cost[i]);
+            if(cost[i]<cost.length-1||cost[i]<cost.length-2){
+                System.out.println(cost[i]);
+            }
         }
     }
 }
