79ea2482ba7a8dc4cd8ea7ddf5bec2fe8fb6945b
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-10-03 17:02:36 -0700

    added new for loop

--- src/LinerSearch.java
+++ src/LinerSearch.java
@@ -10,16 +10,20 @@ public class LinerSearch {
         System.out.println("Enter the element of array List: ");
         for(int i=0;i<count;i++){
             array[i]=items.nextInt();
+            if(array[i]==-1){
+                System.out.println("ELement not present in list");
+            }
         }
         System.out.println("Enter Searched Item: ");
         int search=input.nextInt();
         for(int i=0;i<count;i++){
             if(array[i]==search) {
-                System.out.println("Find the search value at" + i + "Location");
+                System.out.println("Find the search value at: " + i + " Location");
             }
-            else{
-                System.out.println("Item not found");
+            if(array[i]!=search){
+                System.out.println("Item doesn't exist in list.");
             }
+
         }
     }
 }
