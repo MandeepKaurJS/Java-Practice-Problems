1a153b04e980a0a1d95e45b3a837c4a5e994d755
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-10-07 12:29:13 -0700

    Added new java class which implement Bubble Sort algorithm.

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -6,9 +6,17 @@ public class BubbleSort {
         System.out.println("enter the size of Array: ");
         int count=input.nextInt();
         int array[]=new int[count];
+        int temp;
         System.out.println("Enter the elements of Array :");
         for(int i=0;i<count;i++){
             array[i]=input.nextInt();
+            for(int j=0;j<count-i-1;j++) {
+                if (array[i] > array[i + 1]) {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i] = temp;
+                }
+            }
         }
     }
 }
