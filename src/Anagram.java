9bf1404f0e77407e979727d27a61d9b604985700
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-10-07 10:26:32 -0700

    Added new java class which implement Bubble Sort algorithm.

---
+++ src/Anagram.java
@@ -0,0 +1,9 @@
+public class Anagram {
+    public static void main(String[] args){
+        String letter1="Friend";
+        String letter2="Finder";
+        if(letter1.length()==letter2.length()){
+            //return letter1.toLowerCase();
+        }
+    }
+}
