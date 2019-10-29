dcd24cc97c4edb8dc84b51a6c7e268d24d7b28be
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-10-29 16:10:40 -0700

    added new class

--- src/IfElseDemo.java
+++ src/IfElseDemo.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class IfElseDemo {
     public static void main(String[] args ){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Please enter your gards.");
+       int grade=input.nextInt();
+       if(grade>80){
+           System.out.println("welcome to our campus.");
+       }else{
+           System.out.println("PLease try again.");
+       }
 
     }
 }
